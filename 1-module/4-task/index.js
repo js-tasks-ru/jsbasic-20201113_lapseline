15a2cd782a57f20417826fcd7a0a526a90a767f9
Alexey <51817334+Lapseline@users.noreply.github.com>
2020-11-22 23:33:58 +0300

    Решение 'Проверка на спам'

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,12 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+  const preparedString = str.toLowerCase();
+  const subString1 = "1xbet";
+  const subString2 = "xxx";
+  let result =
+    preparedString.includes(subString1) || preparedString.includes(subString2)
+      ? true
+      : false;
+  return result;
 }
